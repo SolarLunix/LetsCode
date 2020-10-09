c5bcce0c3561e73d5a2aa54c04f6ea786914f85a
Melissa <solarlunix@gmail.com>
2020-10-09 10:29:35 +0100

    Lesson 1: Print statements

---
+++ src/TheBasics/PrintStatements.java
@@ -0,0 +1,17 @@
+package TheBasics;
+
+/*******
+ *   LetsCode:TheBasics
+ *   File: PrintStatements
+ *   Created by: SolarLunix
+ *   Created on: 09/10/2020
+ *   Updated on: 09/10/2020
+ *   Project Description: A display of the basic print statements
+ *******/
+public class PrintStatements {
+    public static void main(String[] args) {
+        System.out.print("Hello World \t");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
